ae74ab13d8be99c367988f7f36931238551aa935
GABRIEL CAETANO SIMAO <gabriel.simao@alunos.sc.senac.br>
2025-04-16 20:37:40 -0300

    first commit

---
+++ src/main/java/project/doa/solidario/SolidarioApplication.java
@@ -0,0 +1,13 @@
+package project.doa.solidario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/doa/solidario/SolidarioApplicationTests.java
@@ -0,0 +1,13 @@
+package project.doa.solidario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
