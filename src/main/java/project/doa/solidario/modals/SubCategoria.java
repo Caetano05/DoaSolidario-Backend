0dd42627d1224276de1698a0725eb0f6de09d295
pamella.juca <pamellajucas@gmail.com>
2025-04-23 21:57:12 -0300

    feat: Criação da modal usuario.

--- src/main/java/project/doa/solidario/modals/SubCategoria.java
+++ src/main/java/project/doa/solidario/modals/SubCategoria.java
@@ -1,8 +1,26 @@
 package project.doa.solidario.modals;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class SubCategoria extends EntityId{
 
+    @Column(nullable = false)
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "SubCategoria{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
 }
