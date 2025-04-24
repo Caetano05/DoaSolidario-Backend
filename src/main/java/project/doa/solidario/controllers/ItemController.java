f9d36e9e193c0b18f8cbb6f293b9cde0fa8b5451
caetano05 <caetanogabriel2401@gmail.com>
2025-04-23 21:47:13 -0300

    feat: Criação da classe ItemController

--- src/main/java/project/doa/solidario/controllers/ItemController.java
+++ src/main/java/project/doa/solidario/controllers/ItemController.java
@@ -1,4 +1,48 @@
 package project.doa.solidario.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.doa.solidario.modals.Item;
+import project.doa.solidario.services.ItemService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/item")
 public class ItemController {
+
+    @Autowired
+    private ItemService serviceItem;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Item entity){
+        Item save = serviceItem.salvar(entity);
+        return ResponseEntity.created(URI.create("/api/item/" + entity.getId())).body(save);
+    }
+
+    @GetMapping
+    public  ResponseEntity findAll(){
+        List<Item> itens = serviceItem.buscaTodos();
+        return ResponseEntity.ok(itens);
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity findById(@PathVariable("id") Long id){
+        Item itens = serviceItem.buscarPorId(id);
+        return ResponseEntity.ok().body(itens);
+    }
+    @DeleteMapping("/{id}")
+    public  ResponseEntity remove(@PathVariable("id") Long id){
+        serviceItem.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public  ResponseEntity update(@PathVariable("id") Long id, @RequestBody Item entity){
+        Item ItemAlterado = serviceItem.alterarItem(id, entity);
+        return  ResponseEntity.ok().body(ItemAlterado);
+    }
+
+
 }
