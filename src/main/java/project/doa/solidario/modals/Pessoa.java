e48a1772d46e60736e9fe3dd757b597f2e374a0e
pamella.juca <pamellajucas@gmail.com>
2025-04-23 21:53:10 -0300

    feat: Criação da modal pessoa

--- src/main/java/project/doa/solidario/modals/Pessoa.java
+++ src/main/java/project/doa/solidario/modals/Pessoa.java
@@ -1,7 +1,50 @@
 package project.doa.solidario.modals;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Pessoa extends EntityId{
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Long cpf;
+
+    @Column(nullable = false)
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
