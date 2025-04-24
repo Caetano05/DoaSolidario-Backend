68cfdd65cadcc8360fe2f4e0e7bdc45dcf431db1
caetano05 <caetanogabriel2401@gmail.com>
2025-04-23 21:19:36 -0300

    chore: Alteração no repositorio

--- src/main/java/project/doa/solidario/repositories/ItemRepository.java
+++ src/main/java/project/doa/solidario/repositories/ItemRepository.java
@@ -1,4 +1,9 @@
 package project.doa.solidario.repositories;
 
-public class ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.doa.solidario.modals.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
