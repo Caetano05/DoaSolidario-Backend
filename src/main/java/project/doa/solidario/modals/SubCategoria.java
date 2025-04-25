3cad6abc7b19ed44000da22fd31a4d179c6ef763
caetano05 <caetanogabriel2401@gmail.com>
2025-04-25 20:17:15 -0300

    chore: Ajustes na estrutura da SubCategoria

--- src/main/java/project/doa/solidario/modals/SubCategoria.java
+++ src/main/java/project/doa/solidario/modals/SubCategoria.java
@@ -2,13 +2,23 @@ package project.doa.solidario.modals;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import project.doa.solidario.modals.enums.Categoria;
 
 @Entity
 public class SubCategoria extends EntityId{
 
+    //ATRIBUTOS
     @Column(nullable = false)
     private String descricao;
 
+    //ENUM
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Categoria categoria;
+
+   // GET AND SET
     public String getDescricao() {
         return descricao;
     }
@@ -17,10 +27,20 @@ public class SubCategoria extends EntityId{
         this.descricao = descricao;
     }
 
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    // TOSTRING
     @Override
     public String toString() {
         return "SubCategoria{" +
                 "descricao='" + descricao + '\'' +
+                ", categoria=" + categoria +
                 '}';
     }
 }
