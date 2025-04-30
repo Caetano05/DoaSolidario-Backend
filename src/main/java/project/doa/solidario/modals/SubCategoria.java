6fb148b662ad16c2af41551ec435d77224e04430
caetano05 <caetanogabriel2401@gmail.com>
2025-04-30 19:55:32 -0300

    fix: erro no pull

--- src/main/java/project/doa/solidario/modals/SubCategoria.java
+++ src/main/java/project/doa/solidario/modals/SubCategoria.java
@@ -1,13 +1,10 @@
 package project.doa.solidario.modals;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import project.doa.solidario.modals.enums.Categoria;
+
 
 @Entity
 public class SubCategoria extends EntityId{
-
 
 }
