1fc999957dbc0f7de29b5f87bca0f1c2acc8267a
caetano05 <caetanogabriel2401@gmail.com>
2025-04-30 19:53:48 -0300

    fix: erro no pull

--- src/main/java/project/doa/solidario/modals/SubCategoria.java
+++ src/main/java/project/doa/solidario/modals/SubCategoria.java
@@ -9,38 +9,5 @@ import project.doa.solidario.modals.enums.Categoria;
 @Entity
 public class SubCategoria extends EntityId{
 
-    //ATRIBUTOS
-    @Column(nullable = false)
-    private String descricao;
 
-    //ENUM
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Categoria categoria;
-
-   // GET AND SET
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    // TOSTRING
-    @Override
-    public String toString() {
-        return "SubCategoria{" +
-                "descricao='" + descricao + '\'' +
-                ", categoria=" + categoria +
-                '}';
-    }
 }
